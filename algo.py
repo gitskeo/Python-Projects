da3ac33bfa2071edb875708c536cfb73c1516b05
gitskeo <skeosouvanh@gmail.com>
2022-07-01 15:35:45 -0500

    initial commit

---
+++ algo.py
@@ -0,0 +1,71 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+
+class draw_information:
+    black = 0, 0, 0
+    white = 255, 255, 255
+    green = 0, 255, 0
+    red = 255, 0, 0
+    background_color = white
+
+    side_pad = 100
+    top_pad = 150
+
+    def __init__(self, width, height, lst):
+        self.width = width
+        self.height = height
+
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Sorting Algorithm Project")
+        self.set_list(lst)
+
+    def set_list(self, lst):
+        self.lst = lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
+
+        self.block_width = round((self.width - self.side_pad)/len(lst))
+        self.block_height = round(
+            (self.height - self.top_pad) / (self.max_val - self.min_val))
+        self.start_x = self.side_pad // 2
+
+
+def generate_starting_list(n, min_val, max_val):
+    lst = []
+
+    for _ in range(n):
+        val = random.randint(min_val, max_val)
+        lst.append(val)
+
+    return lst
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    n = 50
+    min_val = 0
+    max_val = 100
+
+    lst = generate_starting_list(n, min_val, max_val)
+    draw_info = draw_information(800, 600, lst)
+
+    while run:
+        clock.tick(60)
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
