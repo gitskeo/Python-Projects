7f88a8dc25c9492cac4c93d0990c57ca369a8af7
gitskeo <skeosouvanh@gmail.com>
2022-09-02 22:14:40 -0500

    added mortality rate

--- API apps/app_covid.py
+++ API apps/app_covid.py
@@ -10,7 +10,6 @@ https://documenter.getpostman.com/view/10808728/SzS8rjbc
 # 2. Convert the data into a useful python objects: usually a list or dictionaries
 
 
-
 url = "https://api.covid19api.com/summary"
 
 
@@ -19,27 +18,43 @@ url = "https://api.covid19api.com/summary"
 #cases = global_stats['TotalConfirmed']
 #mortality_rate = deaths / cases
 
-class Stats():
-
-    def __init__(self, deaths, cases, mortality_rate, global_stats):
-        self.deaths = deaths
-        self.cases = cases
-        self.mortality_rate = mortality_rate
-        self.global_stats = global_stats
 
 class ApiPull():
 
     def __init__(self):
         pass
 
-        
-
     def get_global_stats(self):
         response = requests.get(url)
         data = response.json()
         global_stats = data['Global']
         return global_stats
-    
+
+    def get_deaths(self):
+        deaths = self.get_global_stats()
+        death_stats = deaths['TotalDeaths']
+        return death_stats
+
+    def get_cases(self):
+        cases = self.get_global_stats()
+        case_stats = cases['TotalConfirmed']
+        return case_stats
+
+    # def get_mortality_rate(self):
+    #     deaths = self.get_deaths
+    #     cases = self.get_cases
+    #     print(deaths)
+    #     print(cases)
+    #     mortality_rate = deaths / cases
+    #     return mortality_rate
+
+
 if __name__ == "__main__":
     instance = ApiPull()
     print(instance.get_global_stats())
+    death = (instance.get_deaths())
+    cases = (instance.get_cases())
+    print (cases)
+    print(death)
+    print(death/cases)
+
